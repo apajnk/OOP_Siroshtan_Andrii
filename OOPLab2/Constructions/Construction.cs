7a73325e4fd823b421065b19e4f69740c324c676
Andrii Siroshtan <w69950@student.wsiz.edu.pl>
2024-10-17 11:23:55 +0200

    First commit

---
+++ OOPLab2/Constructions/Construction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLab2.Constructions
+{
+    internal class Construction
+    {
+        public Construction()
+        {
+            
+        }
+        public Construction(float hight, float width, int entrances, int humancapacity, string buildmaterial) 
+        {
+            Height = hight; 
+            Width = width;  
+            Entrances = entrances; 
+            HumanCapacity = humancapacity; 
+            BuildMaterial = buildmaterial;
+        }
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+    }
+}
+//1. Height typu zmiennoprzecinkowego
+//2. Width typu zmiennoprzecinkowego
+//3. Entrances typu liczba całkowita
+//4. HumanCapacity typu liczba całkowita
+//5. BuildMaterial typu tekstowego
+
---
+++ OOPLab2/Program.cs
@@ -0,0 +1,4 @@
+﻿using OOPLab2.Constructions;
+var construction1 = new Construction();
+var construction2 = new Construction(20.0f, 40.5f, 4,4, "Stone");
+Console.WriteLine("Hello Moto!");
